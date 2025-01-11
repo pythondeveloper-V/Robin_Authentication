63d985354f7b7c4e476bfadc93f7732a9b3fa7e0
Python Dev <pythondev@ongraph.com>
2025-01-12 02:02:36 +0530

    Add the robin authentication file

---
+++ Authentication.py
@@ -0,0 +1,42 @@
+from robin_stocks import robinhood
+from signal_sender import send_buy_request,send_sell_request
+
+stocks = ["ZSC"]
+
+
+
+class DummyStrategy:
+    def __init__(self, username, password):
+        try:
+            robinhood.login(username=username, password=password,pickle_path="/home/ubuntu/robin_tokens")
+        except:
+            mfa_code = input("Enter your mfa code here: ")
+            robinhood.login(username=username, password=password,mfa_code=mfa_code,pickle_path="/home/ubuntu/robin_tokens")
+
+    def run(self):
+        for stock in stocks:
+            try:
+                stock_price = robinhood.get_latest_price(stock)
+
+                if stock_price:
+                    half_stock_price = round(float(stock_price[0]) / 2, 2)
+
+                    order = robinhood.order_buy_limit(
+                        symbol=stock, quantity=1, limitPrice=half_stock_price)
+
+                    if order:
+                        print(f"Order was placed for {stock}")
+                    else:
+                        print(f"Order was not placed for {stock}")
+
+            except Exception as e:
+                print(f"Error occurred while placing the order for {stock} with error : {e}")
+
+
+if __name__ == "__main__":
+
+    username = "username"
+    password = "password"
+
+    strategy = DummyStrategy(username=username, password=password)
+    strategy.run()
